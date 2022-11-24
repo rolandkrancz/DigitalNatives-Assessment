764dd49703e09adf409333d7dc47be14cbfd4a80
RoliVegas <roland.krancz@gmail.com>
2022-11-24 15:28:17 +0100

    Converting two digit numbers.

--- js-numerals/converter.js
+++ js-numerals/converter.js
@@ -1,3 +1,23 @@
 export function converter (num) {
-    return 'one';
+    const SINGLES = ['','one','two','three','four','five','six','seven','eight','nine'];
+    const TENS = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    const TEENS = ['','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let result = '';
+
+    const convertTwoDigit = (num) => {
+        let result = '';
+
+        if(num < 10) result = SINGLES[num];
+        else if(num % 10 === 0) result = TENS[num / 10];
+        else if(num < 20) result = TEENS[num % 10];
+        else result = TENS[Math.floor(num / 10)] + '-' + SINGLES[num % 10];
+
+        return result;
+    }
+
+    if(num < 100) {
+        result += convertTwoDigit(num);
+    }
+
+    return result;
 }
--- js-numerals/converter.test.js
+++ js-numerals/converter.test.js
@@ -1,5 +1,29 @@
 import { converter } from "./converter";
 
-test('dummy', () => {
+test('1', () => {
     expect(converter(1)).toEqual('one');
+})
+
+test('7', () => {
+    expect(converter(7)).toEqual('seven');
+})
+
+test('10', () => {
+    expect(converter(10)).toEqual('ten');
+})
+
+test('12', () => {
+    expect(converter(11)).toEqual('eleven');
+})
+
+test('20', () => {
+    expect(converter(20)).toEqual('twenty');
+})
+
+test('21', () => {
+    expect(converter(21)).toEqual('twenty-one');
+})
+
+test('99', () => {
+    expect(converter(99)).toEqual('ninety-nine');
 })
